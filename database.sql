9578e879e16724c1fb3d7a3a97574f04a4086686
Bruno Cavalcante <brunofcavalcante@gmail.com>
2011-09-20 16:18:42 -0300

    database SQL

---
+++ database.sql
@@ -0,0 +1,25 @@
+CREATE TABLE albums (
+    id serial NOT NULL,
+    name character varying NOT NULL,
+    artist_id integer NOT NULL
+);
+
+
+CREATE TABLE artists (
+    id serial NOT NULL,
+    name character varying NOT NULL
+);
+
+ALTER TABLE ONLY albums
+    ADD CONSTRAINT albums_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY artists
+    ADD CONSTRAINT artists_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY albums
+    ADD CONSTRAINT albums_artist_id_fkey FOREIGN KEY (artist_id) REFERENCES artists(id);
+
+
+INSERT INTO albums (id, name, artist_id) VALUES (1, 'Please Please Me', 1);
+
+INSERT INTO artists (id, name) VALUES (1, 'The Beatles');
